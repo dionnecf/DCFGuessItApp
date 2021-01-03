b35f5c6e0022c7e2c30d13b379c290d2e3bcee3e
Dionne Condor-Farrell <info@dcfhosting.co.uk>
2021-01-03 19:57:41 +0000

    Add ScoreViewModel and ScoreViewModelFactory to create the viewModel with a constructor parameter, add the viewModel and factory to the ScoreFragment

--- app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/score/ScoreFragment.kt
@@ -22,6 +22,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.example.android.guesstheword.R
@@ -31,6 +33,9 @@ import com.example.android.guesstheword.databinding.ScoreFragmentBinding
  * Fragment where the final score is shown, after the game is over
  */
 class ScoreFragment : Fragment() {
+
+    private lateinit var scoreViewModel: ScoreViewModel
+    private lateinit var scoreViewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -48,8 +53,22 @@ class ScoreFragment : Fragment() {
 
         // Get args using by navArgs property delegate
         val scoreFragmentArgs by navArgs<ScoreFragmentArgs>()
-        binding.scoreText.text = scoreFragmentArgs.score.toString()
-        binding.playAgainButton.setOnClickListener { onPlayAgain() }
+
+        scoreViewModelFactory = ScoreViewModelFactory(scoreFragmentArgs.score)
+        scoreViewModel = ViewModelProvider(this, scoreViewModelFactory).get(ScoreViewModel::class.java)
+
+        //binding.scoreText.text = scoreFragmentArgs.score.toString()
+        binding.playAgainButton.setOnClickListener { scoreViewModel.playAgain()}
+
+        scoreViewModel.score.observe(this.viewLifecycleOwner, Observer { newFinalScore ->
+            binding.scoreText.text = newFinalScore.toString()
+        })
+
+        scoreViewModel.eventPlayAgain.observe(this.viewLifecycleOwner, Observer { hasEventPlayAgain ->
+            if(hasEventPlayAgain) {
+                onPlayAgain()
+            }
+        })
 
         return binding.root
     }
